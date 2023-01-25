7cab33f33fec7f85bef2619e1d93595c8486618d
Greg <gborota@hotmail.com>
2023-01-24 23:03:04 -0600

    Add audio element to mattai1.html

--- peshitta/books/js/books.js
+++ peshitta/books/js/books.js
@@ -22,3 +22,46 @@ document.getElementById("font-family").addEventListener("change", function (even
     element.style.fontFamily = fontFamily;
   }
 });
+
+function setupLoop(maxVerse) {
+  var startVerse = document.getElementById("start-verse");
+  var endVerse = document.getElementById("end-verse");
+  if (!startVerse || !endVerse) {
+    return;
+  }
+  for (let i = 1; i <= maxVerse; i++) {
+    let opt = document.createElement("option");
+    opt.value = i;
+    opt.id = "start" + i;
+    opt.innerHTML = i;
+    startVerse.appendChild(opt);
+
+    opt = document.createElement("option");
+    opt.id = "end" + i;
+    opt.value = i;
+    opt.innerHTML = i;
+    endVerse.appendChild(opt);
+  }
+  endVerse.value = maxVerse;
+  startVerse.addEventListener("change", function () {
+    let start = parseInt(startVerse.value);
+    let end = parseInt(endVerse.value);
+    if (start > end) {
+      for (let i = 1; i <= maxVerse; i++) {
+        document.getElementById("end" + i).removeAttribute("selected");
+      }
+      document.getElementById("end" + start).setAttribute("selected", true);
+    }
+  });
+
+  endVerse.addEventListener("change", function () {
+    let start = parseInt(startVerse.value);
+    let end = parseInt(endVerse.value);
+    if (start > end) {
+      for (let i = 1; i <= maxVerse; i++) {
+        document.getElementById("start" + i).removeAttribute("selected");
+      }
+      document.getElementById("start" + end).setAttribute("selected", true);
+    }
+  });
+}
