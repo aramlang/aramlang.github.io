efcf8e286c13cc6e434d793aa149dc49b4a11eec
Greg <gborota@hotmail.com>
2023-03-05 03:16:27 -0600

    Adjustments

--- common/js/audio.js
+++ common/js/audio.js
@@ -40,8 +40,8 @@ function setupAudio(
   const speed = document.getElementById('speed');
 
   const hashPrefix = '#ch'         // prefix to prepend to next page hash
-  const startAdjustment = 0.01;    // adjustment for start of loop due to low timerupdate frequency
-  const endAdjustment = 0.01;      // adjustment for end of loop due to low timerupdate frequency
+  const startAdjustment = 0.01;    // some start time tolerance
+  const endAdjustment = 0.01;      // some end time tolerance
   const cues = {};                 // cue dtos
 
   let highlighted = [];            // highlighted elements
