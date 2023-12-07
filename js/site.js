e5038b31c0f9629c117fd11a98c675f186c0a6fc
aramlang <100400031+aramlang@users.noreply.github.com>
2023-12-07 00:48:33 -0600

    more work

--- js/site.js
+++ js/site.js
@@ -27,7 +27,7 @@ window.peshitta || (window.peshitta = {});
   })();
 
   const controls = {};
-  const elemIds = ['fontFamily', 'niqqud', 'stickyHeader', 'bookAndChapter'];
+  const elemIds = ['stickyHeader', 'fontFamily', 'book', 'chapter', 'zawae', 'zawaeLabel'];
   for (let i = 0; i < elemIds.length; i++) {
     const elemId = elemIds[i];
     if (!(controls[elemId] = document.getElementById(elemId))) {
@@ -40,17 +40,16 @@ window.peshitta || (window.peshitta = {});
     verses = peshitta.books[bookNo][chapterNo];
     page.info = {
       book: {
-        a: peshitta.books[0][0],
+        w: peshitta.books[0][0],
         p: peshitta.books[0][1],
         n: bookNo
       },
       chapter: {
-        a: verses[0][0],
+        w: verses[0][0],
         n: chapterNo
       }
     };
 
-    controls.bookAndChapter.innerText = getBookAndChapter(getWordKey());
     for (let i = 1; i < verses.length; i++) {
       var cues = [0];
       page.cues.push(cues);
@@ -80,94 +79,74 @@ window.peshitta || (window.peshitta = {});
     inter: {}
   };
   let verses;
-  const bcCache = {};
   const bookCache = {};
   const chapterCache = {};
-  // Consonants + maqaf
-  const consonants = "\u05d0\u05d1\u05d2\u05d3\u05d4\u05d5\u05d6\u05d7\u05d8\u05d9\u05da\u05db\u05dc\u05dd\u05de\u05df\u05e0\u05e1\u05e2\u05e3\u05e4\u05e5\u05e6\u05e7\u05e8\u05e9\u05ea\u05BE"
-  // Vowels and shin/sin dots and sof pasuq
-  const vowels = "\u05b0\u05b1\u05b2\u05b3\u05b4\u05b5\u05b6\u05b7\u05b8\u05b9\u05ba\u05bb\u05bc\u05c1\u05c2\u05C3"
-  // Compiled Regular Expressions
-  const regexConsonants = new RegExp(`[${consonants}]`, 'gu');
-  const regexVowels = new RegExp(`[${consonants}${vowels}]`, 'gu');
+  const zawaeLabelCache = {};
+  const consonantsRegex = /[\u0710-\u072F]+/gu;
 
-  // Filtering Functions
-  function getConsonants(text) {
+  const getConsonants = (text) => {
     if (typeof text !== 'string') return '';
-    return (text.match(regexConsonants) || []).join('');
+    return (text.match(consonantsRegex) || []).join('');
   }
 
-  function getVoweled(text) {
-    if (typeof text !== 'string') return '';
-    return (text.match(regexVowels) || []).join('');
-  }
-
-  function getBook(key) {
+  const getBook = (key) => {
     let cached = bookCache[key]
     if (cached) {
       return cached;
     }
 
-    const prefix = 'ܟܵܪܘܼܙܘܼܬ݂ܵܐ ܕ';
-    switch (controls.niqqud.value) {
-      case 'vowels':
-        cached = `${getVoweled(page.info.book.a)} ${getVoweled(prefix)} ${page.info.chapter.a}`;
-        break;
-      case 'consonants':
-        cached = `${getConsonants(page.info.book.a)} ${getConsonants(prefix)} ${page.info.chapter.a}`;
-        break;
-      default:
-        cached = `${page.info.book.a} ${prefix} ${page.info.chapter.a}`;
-        break;
-    }
-    bcCache[key] = cached
+    const prefix = 'ܟܵܪܘܼܙܘܼܬ݂ܵܐ';
+    cached = controls.zawae.checked
+      ? `&nbsp;${prefix}&nbsp;ܕ${page.info.book.w}`
+      : `&nbsp;${getConsonants(prefix)}&nbsp;ܕ${getConsonants(page.info.book.w)}`;
+    bookCache[key] = cached
     return cached;
   }
 
-  function getBookAndChapter(key) {
-    let cached = bcCache[key]
+  const getChapter = (key) => {
+    let cached = chapterCache[key]
     if (cached) {
       return cached;
     }
 
-    const pereq = 'פֶּרֶק';
-    switch (controls.niqqud.value) {
-      case 'vowels':
-        cached = `${getVoweled(page.info.book.a)} ${getVoweled(pereq)} ${page.info.chapter.a}`;
-        break;
-      case 'consonants':
-        cached = `${getConsonants(page.info.book.a)} ${getConsonants(pereq)} ${page.info.chapter.a}`;
-        break;
-      default:
-        cached = `${page.info.book.a} ${pereq} ${page.info.chapter.a}`;
-        break;
-    }
-    bcCache[key] = cached
+    const prefix = 'ܨܚܵܚܵܐ';
+    cached = controls.zawae.checked
+      ? `&nbsp;${prefix}&nbsp;${page.info.chapter.w}`
+      : `&nbsp;${getConsonants(prefix)}&nbsp;${getConsonants(page.info.chapter.w)}`;
+    chapterCache[key] = cached
     return cached;
   }
 
-  function getWordKey() {
-    return controls.niqqud.value == 'vowels'
-      ? 'v'
-      : controls.niqqud.value == 'consonants'
-        ? 'c'
-        : 'a';
+  const getZawaeLabel = (key) => {
+    let cached = zawaeLabelCache[key]
+    if (cached) {
+      return cached;
+    }
+
+    const text = 'ܙܵܘܥܹ̈ܐ';
+    cached = controls.zawae.checked
+      ? `&nbsp;${text}`
+      : `&nbsp;${getConsonants(text)}`;
+    zawaeLabelCache[key] = cached
+    return cached;
   }
 
-  function toggleText(event) {
+  const getWordKey = () => controls.zawae.checked ? 'w' : 'c';
+
+  const toggleText = (event) => {
     event.stopImmediatePropagation();
 
     const key = getWordKey();
-    const filter = key == 'v' ? getVoweled : getConsonants;
-
-    controls.bookAndChapter.innerText = getBookAndChapter(key);
+    controls.book.innerHTML = getBook(key);
+    controls.chapter.innerHTML = getChapter(key);
+    controls.zawaeLabel.innerHTML = getZawaeLabel(key);
     for (let i = 1; i < verses.length; i++) {
       let words = verses[i];
       for (let j = 1; j < words.length; j++) {
         let wobj = words[j];
         let word = wobj[key];
         if (!word) {
-          wobj[key] = word = filter(wobj.a);
+          wobj[key] = word = controls.zawae.checked ? wobj.w : getConsonants((wobj.w));
         }
         page.elements.word[i + '-' + j].innerText = word;
       }
@@ -189,7 +168,7 @@ window.peshitta || (window.peshitta = {});
     }
   }, (passiveSupported ? { passive: true } : false));
 
-  controls.niqqud.addEventListener('change', toggleText, (passiveSupported ? { passive: true } : false));
+  controls.zawae.addEventListener('change', toggleText, (passiveSupported ? { passive: true } : false));
 
   controls.stickyHeader.addEventListener('click', (event) => {
     event.stopImmediatePropagation();
