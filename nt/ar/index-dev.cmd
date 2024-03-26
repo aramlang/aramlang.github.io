f4de128c9164cf8ab2e51f5a919bba98d5769427
aramlang <100400031+aramlang@users.noreply.github.com>
2024-03-26 17:49:25 -0500

    Use play/pause icons

--- nt/ar/index-dev.cmd
+++ nt/ar/index-dev.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "%~dp0abun-spoken.html"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "%~dp0index.html"
