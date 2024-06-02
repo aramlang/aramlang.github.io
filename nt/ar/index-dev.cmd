76f58dfb215ad9f17d50b4cb5f81dcfe83bda3ca
aramlang <100400031+aramlang@users.noreply.github.com>
2024-06-02 02:26:38 -0500

    Update index

--- nt/ar/index-dev.cmd
+++ nt/ar/index-dev.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "%~dp001_Mattai_02.html"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "%~dp001_Mattai_12.html"
